d82977b4b8ff8692f191cf7436e561dc328a0a5b
Benny A <virusdownloader7@gmail.com>
2018-03-31 23:38:12 -0700

    Add eval_ctx function to the root

--- src/lib.rs
+++ src/lib.rs
@@ -19,6 +19,12 @@ pub use func::Func;
 pub use expr::{Calculation, Expression, Term};
 pub use context::Context;
 
+/// Parse and evaluate a string
 pub fn eval(expr: &str) -> Result<f64, Error> {
 	Ok(Expression::parse(expr)?.eval()?)
+}
+
+/// Parse and evaluate a string with the given context
+pub fn eval_ctx(expr: &str, ctx: &Context) -> Result<f64, Error> {
+	Ok(Expression::parse_ctx(expr, ctx)?.eval_ctx(ctx)?)
 }
